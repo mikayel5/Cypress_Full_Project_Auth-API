abee2977246127f3f7a145f9a443dfe6937df2b6
mikayel.nahapetyan <mikayel.nahapetyan@softconstruct.com>
2024-01-03 16:03:36 +0400

    CreatingOur Second test

--- cypress/e2e/integration/contact_us.cy.js
+++ cypress/e2e/integration/contact_us.cy.js
@@ -3,12 +3,22 @@
 
 describe("Test Contact Us from via Web", ()=>{
     it("Shuld be able to submit a succsessful submisson via contact us from", ()=>{
-        cy.visit("https://www.webdriveruniversity.com/")
-        cy.get('#contact-us').click()
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+       ///cy.get('#contact-us').click({force: true})
+       cy.get('[name="first_name"]').type("Joe");
+       cy.get('[name="last_name"]').type("blogs");
+       cy.get('[name="email"]').type("joe_blogs123@gmail.com")
+       cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+       cy.get('[type="submit"]').click();
     });
 
 
+    //if we type it.only , only eork second 
     it("Should not be able to sumbit a succesful submission via contact us from as all fields are requeared ",()=>{
-        //cypress code 
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.get('[name="first_name"]').type("Tom");
+        cy.get('[name="last_name"]').type("blogs");
+        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+        cy.get('[type="submit"]').click();
     })
 })
