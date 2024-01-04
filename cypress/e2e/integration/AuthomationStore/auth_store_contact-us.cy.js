e09816d1bb51746dbabd97e3b2365a246b13ae74
mikayel.nahapetyan <mikayel.nahapetyan@softconstruct.com>
2024-01-04 15:45:53 +0400

    chai jQuery

--- cypress/e2e/integration/AuthomationStore/auth_store_contact-us.cy.js
+++ cypress/e2e/integration/AuthomationStore/auth_store_contact-us.cy.js
@@ -11,6 +11,7 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.get('#ContactUsFrm_first_name').type("Joe");
         cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
+        cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
         cy.get("button[title='Submit']").click();
         cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!');
         cy.get('#categorymenu > nav > ul > li:nth-child(6) > a').should('have.length',1)
