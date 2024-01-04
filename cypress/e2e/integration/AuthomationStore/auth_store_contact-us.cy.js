abbf9f4efc521f6ea6aa3eca8102338a770298eb
mikayel.nahapetyan <mikayel.nahapetyan@softconstruct.com>
2024-01-04 13:26:11 +0400

    dynamic and more selectors

--- cypress/e2e/integration/AuthomationStore/auth_store_contact-us.cy.js
+++ cypress/e2e/integration/AuthomationStore/auth_store_contact-us.cy.js
@@ -3,15 +3,42 @@ require('cypress-xpath');
 
 
 describe("Test Contact Us form via Automation Test Store", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
-      //  cy.get("a[href$='contact']").click();
-        cy.xpath('//*[@id="footer"]/footer/section[2]/div/div[1]/div/ul/li[5]/a').click();
+         cy.get("a[href$='contact']").click();
+        //cy.xpath("//a[contains(@href, 'contact')]").click();
+       /// cy.xpath('//*[@id="footer"]/footer/section[2]/div/div[1]/div/ul/li[5]/a').click();
         cy.get('#ContactUsFrm_first_name').type("Joe");
         cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
         cy.get("button[title='Submit']").click(); 
     });
+
+
+    it("Examples of Selectors via WebdriverUni Contact Us Page", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+
+        //By tag name
+        cy.get("input")
+
+        //By attribute name and value
+        cy.get("input[name='first_name']")
+
+        //By id
+        cy.get("#contact_me")
+
+        //By class
+        cy.get(".feedback-input")
+
+        //By multiple classes
+        cy.get("[class='navbar navbar-inverse navbar-fixed-top']")
+
+        //By two different attributes
+        cy.get("[name='email'][placeholder='Email Address']")
+
+        //By xpath
+        cy.xpath("//input[@name='first_name']")
+    })
 })
 
 //*
