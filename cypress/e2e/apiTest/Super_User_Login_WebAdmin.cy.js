5b8c225546fc6313c596f9b52403660e75e9d93b
mikayel.nahapetyan <mikayel.nahapetyan@softconstruct.com>
2024-04-10 17:08:42 +0400

    deosit flow is working

--- cypress/e2e/apiTest/Super_User_Login_WebAdmin.cy.js
+++ cypress/e2e/apiTest/Super_User_Login_WebAdmin.cy.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
 
-const Ajv = require('ajv');
-const avj = new Ajv()
+const Ajv = require("ajv");
+const { UserPromptHandler } = require("selenium-webdriver/lib/capabilities");
+const avj = new Ajv();
 
 describe("Login-WebAdmin", () => {
-
   let AuthToken = null;
 
   it("status code", () => {
@@ -21,6 +21,8 @@ describe("Login-WebAdmin", () => {
       expect(response.headers).to.have.property("authentication");
       AuthToken = response.headers.authentication;
       expect(AuthToken).to.not.be.empty;
+      expect(response.body.AlertType).to.eq("success");
+      expect(response.body.HasError).to.eq(false);
     });
   });
 
@@ -98,7 +100,7 @@ describe("Login-WebAdmin", () => {
     });
   });
 
-var clientData = ""; // Function to get fake user data using cy.request
+  var clientData = ""; // Function to get fake user data using cy.request
 
   function getFakeUserData() {
     return cy
@@ -111,8 +113,8 @@ var clientData = ""; // Function to get fake user data using cy.request
         clientData = response.body.data[0];
         return clientData; // Returning the clientData for further use
       });
-  } 
-  
+  }
+
   function GenereteRandomMail() {
     var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
     var email = "";
@@ -133,25 +135,23 @@ var clientData = ""; // Function to get fake user data using cy.request
   }
   var gender = getRandomInt(2);
 
+  //------Generete Password
+  var password = Math.random().toString(36).slice(2, 10);
 
- //------Generete Password
- var password = Math.random().toString(36).slice(2, 10);
+  //-----Generete Birthdate date random , age above 23
+  var moment = require("moment");
+  var fromDate = moment("1920-01-01");
+  var toDate = moment("2000-01-01");
+  var diffInMilliseconds = toDate.valueOf() - fromDate.valueOf();
+  var randomMilliseconds = Math.random() * diffInMilliseconds;
+  var randomDate = fromDate.valueOf() + randomMilliseconds;
+  var birthDate = moment(randomDate).format("YYYY-MM-DD");
 
- //-----Generete Birthdate date random , age above 23
- var moment = require("moment");
- var fromDate = moment("1920-01-01");
- var toDate = moment("2000-01-01");
- var diffInMilliseconds = toDate.valueOf() - fromDate.valueOf();
- var randomMilliseconds = Math.random() * diffInMilliseconds;
- var randomDate = fromDate.valueOf() + randomMilliseconds;
- var birthDate = moment(randomDate).format("YYYY-MM-DD");
-
- var  birthName= "Leonid-12345";
-
-
+  var birthName = "Leonid-12345";
 
   const createClient = function (firstN, lastN) {
-
+    console.log("firstN ", firstN);
+    console.log("lastN ", lastN);
     cy.request({
       url: "http://localhost:15261/api/en/6/Client/CreateClient",
       method: "POST",
@@ -160,7 +160,7 @@ var clientData = ""; // Function to get fake user data using cy.request
         Currency: "FTN",
         Region: "am",
         BirthName: birthName,
-        FirstName:  firstN,
+        FirstName: firstN,
         LastName: lastN,
         Gender: gender,
         Password: password,
@@ -172,63 +172,354 @@ var clientData = ""; // Function to get fake user data using cy.request
         City: "Tun",
         DocType: "3",
         DocRegionId: "247",
-        RegistrationSource: 99,
-        SwarmRegistrationSource: 100,
       },
     }).then((response) => {
       const schema = {
-        "type": "object",
-        "required": ["StatusCode", "Data"],
-        "properties": {
-          "StatusCode": {"type": "string"},
-          "Data": {
-            "type": "object",
-            "properties": {
-              "UserId": {"type": "integer"},
-              "UserName": {"type": "string"},
-              "CurrencyId":{"type":"string"},
+        type: "object",
+        required: ["StatusCode", "Data"],
+        properties: {
+          StatusCode: { type: "string" },
+          Data: {
+            type: "object",
+            properties: {
+              UserId: { type: "integer" },
+              UserName: { type: "string" },
+              CurrencyId: { type: "string" },
             },
-            "required": ["UserId", "UserName","CurrencyId",]
-          }
+            required: ["UserId", "UserName", "CurrencyId"],
+          },
         },
-        
       }; // end Shcema
-      const validate = avj.compile(schema)
-      const isvalid = validate(response.body)
-      cy.log("Schema validation", isvalid)
-      expect(isvalid).to.be.true
+      const validate = avj.compile(schema);
+      const isvalid = validate(response.body);
+      cy.log("Schema validation", isvalid);
+      //expect(isvalid).to.be.true;
       expect(response.status).to.eq(200);
-        userId = response.body.Data.userId;
-        currencyId = response.body.Data.currencyId;
-        loginEmail = response.body.Data.UserName;
-      cy.log("Status", response.body.StatusCode)
+      userId = response.body.Data.UserId;
+      currencyId = response.body.Data.CurrencyId;
+      loginEmail = response.body.Data.UserName;
+      cy.log("Status", response.body.StatusCode);
       cy.log(`- ResponseBody: ${JSON.stringify(response.body)}`);
     });
   };
-let userId = null;
-let currencyId = null;
-let loginEmail = null;
+  let userId = null;
+  let currencyId = null;
+  let loginEmail = null;
 
-  it.only("Create client and login", () => {
+  it("Create clien", () => {
     getFakeUserData().then((clientData) => {
+      console.log("clientData.firstname ", clientData.firstname);
+      console.log("clientData.lastname ", clientData.lastname);
       createClient(clientData.firstname, clientData.lastname);
     });
   });
 
+  let clientToken = null;
   it("Can Login", () => {
-    console.log("password", password)
     cy.request({
       url: "http://localhost:15261/api/en/6/Client/Login",
       method: "POST",
       body: {
-        Login:  loginEmail,
-        Password:password,
+        Login: loginEmail,
+        Password: password,
         LoginIP: "127.0.0.1",
       },
     }).then((response) => {
       expect(response.status).to.eq(200);
-      console.log("Login-1", response.body.Data.LastName);
+      cy.log(`- ResponseBody: ${JSON.stringify(response.body)}`);
+      clientToken = response.body.Data.Token;
+      cy.log(response.body.AlertMessage);
+      expect(response.status).to.eq(200);
+      //console.log("sdds", response.body.StatusCode)
+      expect(response.body.StatusCode).to.eq("0");
     });
+  });
+
+  it("Update Client Details-verify ", () => {
+    cy.request({
+      url: "http://localhost:2112/api/en/Client/UpdateClientDetails",
+      method: "POST",
+      headers: {
+        Authentication: AuthToken,
+      },
+      body: {
+        Id: userId,
+        CurrencyId: "FTN",
+        Currencies: null,
+        FirstName: "Ansley",
+        LastName: "O'Hara",
+        MiddleName: null,
+        Login: "m9gvtnsy@mail.com",
+        RegionId: 19,
+        Gender: 0,
+        PersonalId: null,
+        Address: "TUnnnn",
+        Email: rEmail,
+        Language: "en",
+        Phone: "5555555",
+        MobilePhone: null,
+        BirthDate: "1949-03-04T00:00:00",
+        TimeZone: null,
+        NickName: null,
+        DocNumber: null,
+        IBAN: null,
+        PromoCode: null,
+        ProfileId: null,
+        MaximalDailyBet: null,
+        MaximalSingleBet: null,
+        CasinoMaximalDailyBet: null,
+        CasinoMaximalSingleBet: null,
+        PreMatchSelectionLimit: 2,
+        LiveSelectionLimit: 1,
+        Excluded: null,
+        ExcludedLocalDate: null,
+        IsSubscribedToNewsletter: false,
+        IsVerified: true,
+        PartnerName: "Vivaro.AGP",
+        PartnerId: 1001,
+        LastLoginIp: "127.0.0.1",
+        YesterdayBalance: null,
+        CreditLimit: 0,
+        IsUsingCredit: false,
+        LastLoginTime: null,
+        LastLoginLocalDate: null,
+        Balance: 0,
+        IsLocked: false,
+        IsCasinoBlocked: null,
+        IsSportBlocked: null,
+        IsRMTBlocked: null,
+        Password: null,
+        SportsbookProfileId: 1,
+        GlobalLiveDelay: 1,
+        Created: "2024-03-15T13:11:23.023+04:00",
+        CreatedLocalDate: "2024-03-15T11:11:23.023",
+        RFId: null,
+        ResetExpireDate: null,
+        ResetExpireDateLocal: null,
+        DocIssuedBy: null,
+        IsUsingLoyaltyProgram: true,
+        LoyaltyPoint: 0,
+        AffilateId: null,
+        BTag: "8s9t545",
+        TermsAndConditionsVersion: null,
+        TCVersionAcceptanceDate: null,
+        TCVersionAcceptanceLocalDate: null,
+        ExcludedLast: null,
+        ExcludedLastLocal: null,
+        UnplayedBalance: 0,
+        IsTest: false,
+        ExternalId: null,
+        AuthomaticWithdrawalAmount: null,
+        AuthomaticWithdrawalMinLeftAmount: null,
+        IsAutomaticWithdrawalEnabled: null,
+        SwiftCode: null,
+        Title: null,
+        BirthCity: null,
+        BirthDepartment: null,
+        BirthRegionId: null,
+        ZipCode: "0054",
+        BirthRegionCode2: null,
+        ActivationCode: null,
+        ActivationCodeExpireDate: null,
+        ActivationCodeExpireDateLocal: null,
+        LastSportBetTime: null,
+        LastSportBetTimeLocal: null,
+        LastCasinoBetTime: null,
+        LastCasinoBetTimeLocal: null,
+        FirstDepositTime: null,
+        FirstDepositDateLocal: null,
+        LastDepositDateLocal: null,
+        LastDepositTime: null,
+        ActivationState: null,
+        ExcludeTypeId: null,
+        DocIssueDate: null,
+        DocIssueCode: null,
+        Province: null,
+        IsResident: false,
+        RegistrationSource: 100,
+        IncomeSource: null,
+        AccountHolder: null,
+        CashDeskId: null,
+        ClientCashDeskName: null,
+        IsSubscribeToEmail: true,
+        IsSubscribeToSMS: true,
+        IsSubscribeToInternalMessage: true,
+        IsSubscribeToPushNotification: true,
+        IsSubscribeToPhoneCall: true,
+        NotificationOptions: 0,
+        IsLoggedIn: false,
+        City: "Tun",
+        CountryName: null,
+        ClientVerificationDate: "2024-03-15T13:30:10.008+04:00",
+        BankName: null,
+        Status: 2,
+        IsNoBonus: false,
+        IsTwoFactorAuthenticationEnabled: false,
+        IsQRCodeUsed: null,
+        PartnerClientCategoryId: null,
+        WrongLoginBlockLocalTime: null,
+        WrongLoginAttempts: 0,
+        PepStatusId: null,
+        SelectedPepStatuses: null,
+        DocRegionId: 247,
+        DocRegionName: null,
+        DocType: 3,
+        DocExpirationDate: null,
+        AMLRisk: null,
+        ExclusionReason: null,
+        Citizenship: null,
+        IsPhoneVerified: false,
+        IsMobilePhoneVerified: false,
+        IsEkengVerified: false,
+        IsEmailVerified: false,
+        OwnerId: null,
+        ChildId: null,
+        BirthName: "Leonid-12345",
+        StatusActiveDate: null,
+        StatusActiveDateLocalTime: null,
+        PartnerFlag: null,
+      },
+    }).then((response) => {
+      cy.log(response.body.AlertMessage);
+      expect(response.status).to.eq(200);
+      expect(response.body.AlertMessage).to.eq(
+        "Operation has completed successfully"
+      );
+      expect(response.body.HasError).to.eq(false);
+    });
+  });
+
+  let canDeposit = null;
+  it("Get Client Restriction ", () => {
+    cy.request({
+      url: `http://localhost:2112/api/null/Client/GetClientRestriction?clientId=${userId}`,
+      method: "GET",
+      headers: {
+        Authentication: AuthToken,
+      },
+      body: {},
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      canDeposit = response.body.Data.CanDeposit;
+      cy.log("sdd-3", response.body.Data.CanDeposit);
+    });
+  });
+
+  function DepositRequest(DepositRequestCount) {
+    console.log(DepositRequestCount);
+    if (DepositRequestCount < 1) {
+      DepositRequestCount = null;
+      cy.log("Condition Failed: Stopping Collection Test");
+      return;
+    }
+    cy.request({
+      url: "http://localhost:15261/api/en/6/ClientRequest/CreateClientRequest",
+      method: "POST",
+      headers: {
+        Token: clientToken,
+      },
+      body: {
+        Amount: "555",
+        Type: "2",
+        PaymentSystemId: "60",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      var DepositRequestAmount = response.body.Data.Amount;
+      cy.log("Amount", DepositRequestAmount);
+      const StatusCode = response.body.StatusCode
+
+      if (DepositRequestAmount !== undefined) {
+        cy.log(
+          "Deposit Request created - Request Amount-" + DepositRequestAmount
+        );
+        const newNumber = DepositRequestCount - 1;
+        DepositRequest(newNumber);
+  
+      }
+      else if (StatusCode  !==0){
+        it("Save Client Restriction ", () => {
+          cy.request({
+            url: "http://localhost:2112/api/en/Client/SaveClientRestriction",
+            method: "POST",
+            headers: {
+              Authentication: AuthToken,
+            },
+            body: {
+              ClientId: UserId,
+              CanLogin: true,
+              CanBet: true,
+              CanDeposit: true,
+              CanWithdraw: false,
+              CanIncreaseLimit: true,
+              CanClaimBonus: true,
+              CanCasinoLogin: false,
+            },
+          }).then((response) => {
+            expect(response.status).to.eq(200);
+            const StatusCode = response.body.StatusCode;
+            const AlertMessageValue = responseData.AlertMessage;
+            cy.log("Client Restriction changed", AlertMessageValue);
+            expect(AlertMessageValue).to.eq(
+              "Operation has completed successfully"
+            );
+            cy.log();
+          });
+        });
+      }
+      else{
+        it ("Get Client Deposit Requests With Totals", () =>{
+          cy.request({
+            url: "http://localhost:2112/api/null/Client/GetClientDepositRequestsWithTotals",
+            method: "POST",
+            headers: {
+              Authentication: AuthToken,
+            },
+            body:{
+              ClientId: UserId,
+            }
+          })
+        })
+      }
+    });
+
+    
+  }
+
+  it("Create Client Request-Deposit/Withdraw ", () => {
+    if (canDeposit === true) {
+      DepositRequest(0);
+    } else {
+      console.log("say hi");
+      it("Save Client Restriction ", () => {
+        cy.request({
+          url: "http://localhost:2112/api/en/Client/SaveClientRestriction",
+          method: "POST",
+          headers: {
+            Authentication: AuthToken,
+          },
+          body: {
+            ClientId: UserId,
+            CanLogin: true,
+            CanBet: true,
+            CanDeposit: true,
+            CanWithdraw: false,
+            CanIncreaseLimit: true,
+            CanClaimBonus: true,
+            CanCasinoLogin: false,
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          const StatusCode = response.body.StatusCode;
+          const AlertMessageValue = responseData.AlertMessage;
+          cy.log("Client Restriction changed", AlertMessageValue);
+          expect(AlertMessageValue).to.eq(
+            "Operation has completed successfully"
+          );
+          cy.log();
+        });
+      });
+    }
   });
 
   // it("Can Deposit ", () => {
